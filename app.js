e6f3a0690651b5c195f13d75a78b6c05c41378d0
Nerdvona <nerdvona4@gmail.com>
2020-12-05 23:59:51 -0600

    Added change theme functionality to modal change theme buttons and styled accordingly. Changed modal form to modal buttons for better functionality. Changed .title spans from id to class. Changed the name of color functions (to better simplify it). Little changes to css comments. New css variable (--main-transition = 0.5s). Added color gradient border to change theme modal.

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-function popUp() {
+function modalShow() {
     document.getElementById("modal-container").style.display = "block";
     document.getElementById("overlay").style.display = "block";
 }
+
+function closeModal() {
+    document.getElementById("modal-container").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
+
+// Change Color Functions
+function green() {
+    document.body.classList.remove('orange-mode');
+    document.body.classList.remove('dark-mode');
+    document.body.classList.toggle('green-mode');
+}
+
+function orange() {
+    document.body.classList.remove('green-mode');
+    document.body.classList.remove('dark-mode');
+    document.body.classList.toggle('orange-mode');
+}
+
+function dark() {
+    document.body.classList.remove('green-mode');
+    document.body.classList.remove('orange-mode');
+    document.body.classList.toggle("dark-mode");
+}
+
+
+// Stopwatch
